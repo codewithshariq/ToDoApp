764bae69616f394ef87afb5ae385aaf5d1d3006f
Shariq Ahmed Khan <shariq@carbonteq.com>
2021-06-09 21:22:17 +0500

    tag: Added jwt config file

--- config/sql.js
+++ config/sql.js
@@ -1,23 +1,9 @@
 const sqlConfig = {
   HOST: "localhost",
-
-  USER: "carbonteq",
-
-  PASSWORD: "123456",
-
+  USER: "root",
+  PASSWORD: "12345",
   DB: "testdb",
-
   dialect: "mysql",
-
-  pool: {
-    max: 5,
-
-    min: 0,
-
-    acquire: 30000,
-
-    idle: 10000,
-  },
 };
 
 module.exports = sqlConfig;
