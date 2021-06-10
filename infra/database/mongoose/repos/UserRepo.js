16fcd47fc70071d092ac2bd892b880627e9c182d
Shariq Ahmed Khan <shariq@carbonteq.com>
2021-06-10 17:59:47 +0500

    fix: added two new methods in user repo

--- infra/database/mongoose/repos/UserRepo.js
+++ infra/database/mongoose/repos/UserRepo.js
@@ -1,20 +1,36 @@
 const userModel = require("../models/user");
 
 class UserRepo {
-  static async getUser({ id }) {
-    return await userModel.findById(id).exec();
+  async getUserByEmail(email) {
+    let user = await userModel.findOne({ email: email }).exec();
+    if (user) {
+      return user;
+    } else {
+      let err = new Error();
+      err.name = "unregistered_user";
+      err.message = "User with the given email is not registered";
+      throw err;
+    }
   }
-  static async createUser({ name, email }) {
+  async getUserById(id) {
+    let user = await userModel.findById(id).exec();
+    if (user) {
+      return user;
+    } else {
+      throw new Error("User with the given ID does not exist");
+    }
+  }
+  async createUser(name, email) {
     return await userModel.create({ name, email });
   }
-  static async updateUser({ id, name }) {
+  async updateUser({ id, name }) {
     let user = await userModel.findById(id).exec();
     if (user) {
       user.name = name;
       return await user.save();
     }
   }
-  static async deleteUser({ id }) {
+  async deleteUser({ id }) {
     let user = await userModel.findByIdAndDelete(id).exec();
     if (user) {
       return user;
