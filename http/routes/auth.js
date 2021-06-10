0a93833929048d4b979868d918b041607db6d411
Shariq Ahmed Khan <shariq@carbonteq.com>
2021-06-10 18:05:01 +0500

    fix: added routes for login

--- http/routes/auth.js
+++ http/routes/auth.js
@@ -1,14 +1,20 @@
 const express = require("express");
 var router = express.Router();
-const Controller = require("../controllers/AuthController");
-const authController = new Controller();
+const AuthController = require("../controllers/AuthController");
+const UserController = require("../controllers/UserController");
+const authController = new AuthController();
+const userController = new UserController();
 
 router.get("/getUrl", async (req, res) => {
   authController.generateUrl(req, res);
 });
 
 router.get("/google/callback", async (req, res) => {
-  authController.createUser(req, res);
+  authController.googleAuth(req, res);
+});
+
+router.post("/login", async (req, res) => {
+  authController.loginUser(req, res);
 });
 
 module.exports = router;
